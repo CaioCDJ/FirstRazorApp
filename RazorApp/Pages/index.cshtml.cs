c6e3d5033be197992dd5e8ebff8dd826218c125e
CaioCDJ <caiodjesus1@hotmail.com>
2023-03-05 20:26:03 -0300

    Project Init

---
+++ RazorApp/Pages/index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages;
+
+public class Index : PageModel
+{
+  // Executado quando a pasta for renderizada
+  public void OnGet(){}
+  
+  // post kek
+  public void OnPost(){}
+  
+}
