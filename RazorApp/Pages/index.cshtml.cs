9d169e0196ecde605b4d476641b02782934d09f1
CaioCDJ <caiodjesus1@hotmail.com>
2023-03-05 21:17:59 -0300

    manipulating varibles

--- RazorApp/Pages/index.cshtml.cs
+++ RazorApp/Pages/index.cshtml.cs
@@ -4,10 +4,23 @@ namespace RazorApp.Pages;
 
 public class Index : PageModel
 {
+
+  public List<Clubes> clubes { get; set; } =  new ();// inicialização anonima
+
   // Executado quando a pasta for renderizada
-  public void OnGet(){}
+  public async Task OnGet(){
+  
+    // deixa o navegador em estado de loading
+    await Task.Delay(5000);
+
+    clubes.Add(new Clubes("Peixão Soccer Club", "Santos"));
+    clubes.Add(new Clubes("Black Bridge", "Campinas"));
+    clubes.Add(new Clubes("Fortress", "Fortaleza"));
+  }
   
   // post kek
   public void OnPost(){}
   
 }
+
+public record Clubes(string nome,string cidade );
